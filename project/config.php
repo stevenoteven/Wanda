c9b403c887b7e8d55304d121f3892a1f6c1ad8b1
stevenoteven <44447619+stevenoteven@users.noreply.github.com>
2021-05-21 17:31:53 +0300

    Add files via upload
    All of the files used in my project. Some may be out of order, but I'm working on that now.

---
+++ project/config.php
@@ -0,0 +1,6 @@
+<?php
+define('C:\xampp\htdocs\project', __DIR__);
+require_once __DIR__."/localhost/project/config.php";
+...
+require_once SITE_ROOT."/localhost/project/save.php";
+?>
---
+++ project/save3.php
@@ -0,0 +1,57 @@
+<?php
+
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$db = "project";
+
+//connect to the database using procedural method
+
+$dbconnect = mysqli_connect($hostname,$username,$password,$db);
+
+if(!$dbconnect){
+	
+	echo "an error occured while connecting to the database";
+	
+}
+
+if(isset($_POST['save'])){
+//get values from the form
+$converse = $_POST['converse'];
+
+$sqlinsert = "INSERT INTO converse(conversation) VALUES('$converse')";
+//write the SQL STATEMENT FOR SAVING - INSERT
+
+if(mysqli_query($dbconnect,$sqlinsert)){
+	
+	echo "<script type='text/javascript'> alert('Data saved successfully');</script>";
+	header('location: index.php');
+}
+else{
+	
+	echo "<script type='text/javascript'> alert('Your comment has not been posted, please try again later');</script>";
+}
+
+//close the connection
+
+mysqli_close($dbconnect);
+
+}
+else{
+	
+	header('location: index.php');
+}
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
